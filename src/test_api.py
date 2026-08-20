2056b8b31a7c56c1aeb3dc9b5797a11fb4bfa746
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-08-20 13:51:20 +0100

    save bootstrap static to repo

--- src/test_api.py
+++ src/test_api.py
@@ -4,9 +4,21 @@ import pandas as pd
 
 pd.set_option('display.max_columns', None)
 
-base_url = 'https://fantasy.premierleague.com/api/'
-r = requests.get(base_url + 'bootstrap-static/').json()
+#base_url = 'https://fantasy.premierleague.com/api/'
+#r = requests.get(base_url + 'bootstrap-static/').json()
 #pprint(r, indent=2, depth=1, compact=True)
 
+#with open('bootstrap-static.json', 'w') as f:
+#    json.dump(r, f)
+
+with open('bootstrap-static.json') as f:
+    r = json.load(f)
+
 players = pd.json_normalize(r['elements'])
-pprint(players[0])
+pprint(players[['id', 'web_name', 'team', 'element_type']])
+
+print(players.columns)
+
+teams = pd.json_normalize(r['teams'])
+
+print(teams.head())
