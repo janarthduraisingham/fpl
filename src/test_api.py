e11cf171ccecd1579aa1e06daa3c12f36548bd23
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-08-20 13:42:39 +0100

    uv add python

--- src/test_api.py
+++ src/test_api.py
@@ -1,2 +1,12 @@
 import requests, json
 from pprint import pprint
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
+base_url = 'https://fantasy.premierleague.com/api/'
+r = requests.get(base_url + 'bootstrap-static/').json()
+#pprint(r, indent=2, depth=1, compact=True)
+
+players = pd.json_normalize(r['elements'])
+pprint(players[0])
