289fc339e3e2ea97f86c9e081647a194622a2062
janarthduraisingham <51697690+janarthduraisingham@users.noreply.github.com>
2026-09-06 19:50:57 +0100

    add code to determine underpriced players (#4)

--- src/test_api.py
+++ src/test_api.py
@@ -1,6 +1,7 @@
 import requests, json
 from pprint import pprint
 import pandas as pd
+import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', None)
 
@@ -17,9 +18,9 @@ with open('bootstrap-static.json') as f:
 players = pd.json_normalize(r['elements']).rename(
     columns={'id':'PID'}
 )
-pprint(players[['PID', 'web_name', 'team', 'element_type']])
+#pprint(players[['PID', 'web_name', 'team', 'element_type']])
 
-print(players.columns)
+#print(players.columns)
 
 teams = pd.json_normalize(r['teams'])
 
@@ -43,15 +44,67 @@ df = df.merge(
     }
 )
 
-print(df[['PID', 'first_name', 'second_name', 'team_name', 'position_name']].head(20))
-
+#print(df[['PID', 'first_name', 'second_name', 'team_name', 'position_name', 'now_cost', 'total_points']].head(20))
+#for col in df.columns:
+#    print(col)
 # For each gameweek, get all player data: https://fantasy.premierleague.com/api/event/{GID}/
 # For each player, get gameweeek history: https://fantasy.premierleague.com/api/element-summary/{PID}
 pids = df['PID']
+#i=0
+#for pid in pids:
+#    i+=1
+#    print(str(i)+" of "+str(len(df['PID']))+" Loading historical data for "+df[df['PID']==pid]['web_name'].item()+"...")
+#    pid_gameweek_history = requests.get(base_url + 'element-summary/' + str(pid) + '/').json()
 
-for pid in pids:
-    print(pid)
-    pid_gameweek_history = requests.get(base_url + 'element-summary/' + str(pids[0]) + '/').json()
+#    with open('data/element_summary/element_summary_' + str(pid) + '.json', 'w') as f:
+#        json.dump(pid_gameweek_history, f)
 
-    with open('data/element_summary/element_summary_' + str(pid) + '.json', 'w') as f:
-        json.dump(pid_gameweek_history, f)
+
+
+
+df['points_per_cost'] = df['total_points']/df['now_cost']
+
+
+#df['points_per_cost'].hist(bins=100)
+#plt.ylabel('Frequency')
+#plt.title('Histogram of col')
+#plt.show()
+
+(df[df['points_per_cost']>2]).sort_values(
+    ['position_name', 'points_per_cost'], ascending=[True, False])[
+        ['first_name', 'second_name', 'team_name', 'position_name', 'now_cost', 'total_points', 'points_per_cost']
+    ].to_csv(
+        "output/1yr_underpriced.csv")
+
+def load_element_summary(pid):
+    with open('data/element_summary/element_summary_'+str(pid)+'.json') as f:
+        r = json.load(f)
+    return(r)
+
+
+def last_2_yrs_points(pid):
+    pl = pd.json_normalize(load_element_summary(pid)['history_past'])
+    if 'season_name' in pl.columns:
+        recent_pl = pl[pl['season_name'] >= '2024/25']
+        last_2_year_points = recent_pl['total_points'].sum()
+        return(last_2_year_points)
+    else:
+        return(0)
+
+#print(last_2_yrs_points(53))
+
+#i = 0
+#for pid in pids:
+#    i+=1
+#    print(str(i) + " pid: " + str(pid) + " " + str(last_2_yrs_points(pid)))
+
+df['points_2yr'] = df['PID'].apply(last_2_yrs_points)
+df['points_per_cost_2yr'] = df['points_2yr'] / df['now_cost']
+
+df.sort_values(
+    ['position_name', 'points_per_cost_2yr'], ascending=[True, False])[
+        ['first_name', 'second_name', 'team_name', 'position_name', 'now_cost', 'total_points', 'points_per_cost',
+         'points_2yr',
+         'points_per_cost_2yr']
+    ].to_csv(
+        "output/underpriced.csv")
