0a2c786bd1a37221aa7b07afcdb488df8f0f43a7
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-09-24 17:49:35 +0100

    rank all players by ewmac and sma_4

--- src/fpl/ewmac.py
+++ src/fpl/ewmac.py
@@ -16,16 +16,20 @@ def points_ts(pid):
     ]
 
     # Join names, teams, positions
-    names = players()[['PID', 'first_name', 'second_name']]
+    names = players()[['PID', 'first_name', 'second_name', 'element_type']]
 
     df = pd.merge(
         left = df,
         right = names,
         left_on = "element",
         right_on = "PID"
-    ).drop(columns=['PID']).rename(
-        columns={'round':'GW'}
-    )
+    ).drop(columns=['PID']).merge(
+        positions()[['id', 'singular_name']],
+            left_on='element_type',
+            right_on='id').rename(
+        columns={'round':'GW',
+                 "singular_name":"position_name"}
+            )
 
     ax = sns.lineplot(data=df, x='GW', y='total_points')
     plot = ax.figure
@@ -35,17 +39,20 @@ def points_ts(pid):
               'plot':plot}
     return(result)
 
-def ewmac_ts(pid, short_alpha, long_alpha):
+def ewmac_ts(pid, short_alpha=0.5, long_alpha=0.25):
     df = points_ts(pid)['df']
     df['ewma_'+str(short_alpha)] = df['total_points'].ewm(alpha=short_alpha).mean()
     df['ewma_'+str(long_alpha)] = df['total_points'].ewm(alpha=long_alpha).mean()
 
-    df_tidy = df[['GW', 'total_points', 'ewma_'+str(short_alpha), 'ewma_'+str(long_alpha)]].melt(
+    df['ewmac_'+str(short_alpha)+'_'+str(long_alpha)] = df['ewma_'+str(short_alpha)] - df['ewma_'+str(long_alpha)]
+    df['sma_4'] = df['total_points'].rolling(4).mean()
+
+
+    df_tidy = df[['GW', 'total_points', 'ewma_0.5', 'ewma_0.25', 'ewmac_0.5_0.25', 'sma_4']].melt(
             id_vars = 'GW',
             var_name= 'statistic',
             value_name= 'value'
         )
-
     ax = sns.lineplot(data=df_tidy, x='GW', y='value', hue='statistic')
     plot = ax.figure
     plt.close(plot)
@@ -56,5 +63,28 @@ def ewmac_ts(pid, short_alpha, long_alpha):
 
     return(result)
 
+
+def ewmac_all(gw):
+
+    rows = []
+
+    for pid in players()['PID']:
+        #print(pid)
+        df = ewmac_ts(pid=pid,
+                      short_alpha=0.5,
+                      long_alpha=0.25)['df']
+        df = df[df['GW']==gw]
+        rows.append(df)
+
+    df = pd.concat(rows, ignore_index=True).sort_values(
+        by=['sma_4', 'ewmac_0.5_0.25'],
+        ascending=[False, False]
+    )[
+        ['first_name', 'second_name', 'position_name', 'sma_4', 'ewmac_0.5_0.25']
+    ]
+
+    return(df)
+
 if __name__ == '__main__':
-    print(ewmac_ts(pid=10, short_alpha=0.5, long_alpha=0.25)['df'])
+    #print(ewmac_df'])
+    print(ewmac_all(gw=5))
